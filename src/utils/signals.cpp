7e6540c943d3e46c99cd1d6a5b97de606af91753
triedel42 <145435524+triedel42@users.noreply.github.com>
2025-06-05 11:21:35 +0200

    Remove threads, Set up SocketHandler

--- src/utils/signals.cpp
+++ src/utils/signals.cpp
@@ -1,5 +1,7 @@
 #include "Webserv.hpp"
 
+volatile sig_atomic_t g_stop;
+
 void signalHandler(int signum) {
     std::cout << "Signal (" << signum << ") received." << std::endl;
     std::cout << "Stopping all servers..." << std::endl << std::endl;
