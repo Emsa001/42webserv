cfe3a400a9bb0ee787a0c40aee285253485ea2d8
Emanuel <emikscur123@gmail.com>
2025-07-12 14:04:12 +0200

    cookie testm t

--- tests/cookies.cpp
+++ tests/cookies.cpp
@@ -8,7 +8,7 @@ TEST(WebservCookiesTests, BasicCookieTest)
     Server server(config.getServers()[0].getMap());
 
     HttpRequest request(
-        "GET / HTTP/1.1\r\n"
+        "GET /cookies HTTP/1.1\r\n"
         "Host: MainServer\r\n"
         "Cookie: test_cookie=12345\r\n"
         "Connection: close\r\n\r\n"
@@ -22,7 +22,37 @@ TEST(WebservCookiesTests, BasicCookieTest)
 
     // Ensure the cookie did not change (no Set-Cookie in response)
     std::string raw = response.getResponse();
-    EXPECT_TRUE(raw.find("Set-Cookie:") == std::string::npos);
+    EXPECT_TRUE(raw.find("Set-Cookie: test_cookie=12345;") != std::string::npos);
+
+    // Optionally, check if body contains some static content (e.g., index.html)
+    EXPECT_TRUE(raw.find("<html") != std::string::npos || raw.find("<!DOCTYPE") != std::string::npos);
+}
+
+TEST(WebServerCookiesTests, SettingMultipleCookies)
+{
+    Config &config = Config::instance();
+    config.parse("conf/default.yml");
+    Server server(config.getServers()[0].getMap());
+
+    HttpRequest request(
+        "GET /cookies/test.py HTTP/1.1\r\n"
+        "Host: MainServer\r\n"
+        "Connection: close\r\n\r\n",
+        server.getConfig()
+    );
+
+    HttpResponse response = server.handleResponse(&request);
+
+    // Verify status
+    EXPECT_EQ(response.getStatusCode(), 200);
+    EXPECT_TRUE(response.getStatusLine().find("200 OK") != std::string::npos);
+
+    // Ensure the cookie did not change (no Set-Cookie in response)
+    std::string raw = response.getResponse();
+    EXPECT_TRUE(raw.find("Set-Cookie: test_cookie=123;") != std::string::npos);
+    EXPECT_TRUE(raw.find("Set-Cookie: sessionid=abc123;") != std::string::npos);
+    EXPECT_TRUE(raw.find("Set-Cookie: user_pref=dark_mode;") != std::string::npos);
+
 
     // Optionally, check if body contains some static content (e.g., index.html)
     EXPECT_TRUE(raw.find("<html") != std::string::npos || raw.find("<!DOCTYPE") != std::string::npos);
